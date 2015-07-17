4d7a92bfe292daffbb7a35bb074765b0301c47c6
Nazar Gargol <nazargargol@gmail.com>
2015-07-17 22:09:12 +0200

    adds test coverage check

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      './src/*.js': ['coverage']
+    },
+
+    coverageReporter: {
+      type : 'lcovonly',
+      dir : 'coverage/'
+    },
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
